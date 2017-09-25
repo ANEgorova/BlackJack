fc532987e10616ebff6c551445fce092774093bf
Alena Egorova <alena.egorova@phystech.edu>
2017-09-25 18:54:17 +0300

    added bet and money

--- BJmain.rb
+++ BJmain.rb
@@ -6,6 +6,7 @@ $current_index = 0
 $cards_indexes = Array.new
 $user = Hash.new
 $dealer = Hash.new
+$user['money'] = 1000
 
 def get_cards_indexes
   return (1..52).to_a.sample 52
@@ -37,12 +38,12 @@ def init_state(player, is_user)
   # TODO: Loop
   cards << get_card_by_index($cards_indexes[$current_index])
   hand << prettify_card(cards.last)
-  update_sum(player, cards.last)
+  update_score(player, cards.last)
   $current_index += 1
   cards << get_card_by_index($cards_indexes[$current_index])
   hand << prettify_card(cards.last)
   if is_user
-    update_sum(player, cards.last)
+    update_score(player, cards.last)
   end
   $current_index += 1
   player['cards'] = cards
@@ -59,7 +60,7 @@ def print_scores
 end
 
 def print_options_choosing
-  options_string = "Choose one option: [1]Hit [2]Stand"
+  options_string = 'Choose one option: [1]Hit [2]Stand'
   option_index = 3
   if $user['cards'].size == 2
     options_string += " [#{option_index}]Double_down [#{option_index + 1}]Surrender"
@@ -78,13 +79,14 @@ def add_new_card(player)
   new_card = get_card_by_index($cards_indexes[$current_index])
   player['cards'] << new_card
   player['hand'] << prettify_card(new_card)
-  update_sum(player, new_card)
+  update_score(player, new_card)
   $current_index += 1
   puts '---------------'
   puts "New card is: #{prettify_card(new_card)}"
+  puts '---------------'
 end
 
-def update_sum(player, card)
+def update_score(player, card)
   player['score'] += card < 10 ? card : 10
 end
 
@@ -103,58 +105,106 @@ def init_table
   $cards_indexes = get_cards_indexes
   init_state($user, true)
   init_state($dealer,false)
+  $user['bet'] = 0
 end
 
-def get_winner
-  if $dealer['score'] == $user['score']
+def get_winner(user)
+  if $dealer['score'] == user['score']
     puts 'Push! NOBODY LOSE!'
+    update_money(user, false, true)
   elsif $dealer['score'] > 21
     puts 'YOU WIN! DEALER LOSE!' # print score????
+    update_money(user, true, false)
   elsif get_dealer_diff < get_user_diff
     puts 'YOU LOSE! DEALER WIN'
+    update_money(user, false, false)
   else
     puts 'YOU WIN! DEALER LOSE!'
+    update_money(user, true, false)
   end
 end
 
-def dealer_game
+def dealer_game(user)
   $is_first = false
-  puts "Your score is #{$user['score']}!"
+  puts "Your score is #{user['score']}!"
   puts "Dealer's turn:"
-  update_sum($dealer, $dealer['cards'][1])
+  update_score($dealer, $dealer['cards'][1])
   print_scores
   while $dealer['score'] < 17
     sleep(1)
     add_new_card($dealer)
-    puts "Your score: #{$user['score']}; Dealer score: #{$dealer['score']}"
+    puts "Your score: #{user['score']}; Dealer score: #{$dealer['score']}"
   end
-  get_winner
+  get_winner(user)
 end
 
 def play_again
   puts 'Do you want to play again? ([Y]es/[N]o)'
   new_game_decision = gets.chomp
   if new_game_decision.match('y|Y|Yes')
-    init_table
-    game($user)
+    main
   else
-    puts 'You money: 0'
-    print 'Thanks for game! Goodbye!'
+    puts "Your money now is: #{$user['money']}$"
+    print 'Thanks for a game! Goodbye!'
     return false
   end
+end
+
+def process_bet (player, bet)
+  case bet
+    when 1
+      player['bet'] = 10
+      player['money'] -= 10
+    when 2
+      player['bet'] = 25
+      player['money'] -= 25
+    when 3
+      player['bet'] = 100
+      player['money'] -= 100
+    when 4
+      player['bet'] = 200
+      player['money'] -= 200
+    when 5
+      player['bet'] = 500
+      player['money'] -= 500
+    else
+      player['bet'] = bet
+      player['money'] -= bet
+  end
+  puts "You made #{player['bet']}$ bet!"
+end
+
+def update_money(player, is_winner, is_push)
+  if is_push
+    player['money'] += $user['bet']
+  end
+  if is_winner
+    player['money'] += ($user['bet'] * 2)
+  end
+  player['bet'] = 0
+  puts "Your money now is: #{player['money']}$"
+end
+
+def print_bet_choosing
+  puts 'Make a bet: [1] 10$, [2] 25$, [3] 100$, [4] 200$, [5] 500$'
+  puts 'Please, enter number or money amount without $ sign'
+  return gets.chomp.to_i
 end
 
 def game(user)
   if user['score'] == 21
     puts 'You got 21!'
     puts "Dealer's second card:"
-    update_sum($dealer, $dealer['cards'][1])
+    update_score($dealer, $dealer['cards'][1])
     $is_first = false
     print_scores
     if $dealer['score'] == 21
       puts 'PUSH! NOBODY WINS!'
+      update_money(user, false, true)
     else
       puts 'BLACK JACK!'
+      puts "You win #{$user['bet']}"
+      update_money(user, true, false)
     end
     unless play_again
       return
@@ -163,7 +213,7 @@ def game(user)
   while true
     print_scores
     if user['score'] == 21
-      dealer_game
+      dealer_game(user)
       unless play_again
         break
       end
@@ -175,6 +225,7 @@ def game(user)
       if user['score'] > $winner_score
         puts "Your score is: #{user['score']}"
         puts 'YOU LOSE! DEALER WIN'
+        update_money(user, false, false)
         unless play_again
           break
         end
@@ -182,7 +233,7 @@ def game(user)
         next
       end
     elsif option.match('2|Stand')
-      dealer_game
+      dealer_game(user)
       unless play_again
         break
       end
@@ -198,7 +249,7 @@ def game(user)
           break
         end
       else
-        dealer_game
+        dealer_game(user)
         unless play_again
           break
         end
@@ -213,9 +264,9 @@ def game(user)
       split_user['cards'] << user['cards'].pop
       split_user['hand'] << user['hand'].pop
       split_user['score'] = 0
-      update_sum(split_user, split_user['cards'][0])
+      update_score(split_user, split_user['cards'][0])
       user['score'] = 0
-      update_sum(user, user['cards'][0])
+      update_score(user, user['cards'][0])
       puts 'GAME FOR ONE HAND:'
       game(user)
 
@@ -228,7 +279,13 @@ def game(user)
   end
 end
 
-init_table
-puts "Your money is 0"
-puts "Make bet"
-game($user)
+def main
+  init_table
+  bet = print_bet_choosing
+  process_bet($user, bet)
+  game($user)
+end
+
+puts 'Welcome to BlackJack'
+puts 'Your money now is 1000$'
+main
